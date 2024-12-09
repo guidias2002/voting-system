1ef104d002351b31c14b82078ecfe28d8a6e23d4
guidias2002 <guidias200220@gmail.com>
2024-12-09 10:22:33 -0300

    feat: VotingRepository created

---
+++ src/main/java/com/gcd/voting_system/repository/VotingRepository.java
@@ -0,0 +1,14 @@
+package com.gcd.voting_system.repository;
+
+import com.gcd.voting_system.entity.VotingEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface VotingRepository extends JpaRepository<VotingEntity, Long> {
+    List<VotingEntity> findAllByExpirationTimeBeforeAndStatus(LocalDateTime now, boolean status);
+
+}
