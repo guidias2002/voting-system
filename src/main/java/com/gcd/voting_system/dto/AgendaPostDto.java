d682eaaa9d8efec331f88cfd48c341930cee68cc
guidias2002 <guidias200220@gmail.com>
2024-12-09 10:21:22 -0300

    feat: AgendaPostDto created

---
+++ src/main/java/com/gcd/voting_system/dto/AgendaPostDto.java
@@ -0,0 +1,15 @@
+package com.gcd.voting_system.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AgendaPostDto {
+
+    private String agenda;
+
+    public String getAgenda() {
+        return agenda;
+    }
+}
